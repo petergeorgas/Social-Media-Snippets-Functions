dd0c4cd57f058e2ce76f77744ecddd8909ba0dff
Peter Georgas <peter_georgas@yahoo.com>
2021-12-23 15:53:04 -0500

    Changed over to TypeScript

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -1,10 +1,13 @@
+import type {VercelRequest, VercelResponse} from "@vercel/node"
+
 /*
     Serverless function designed to retrieve information regarding a specific Tweet from the Twitter API. 
     It is preferred to use a serverless function here because we have to use an authorization header in order
     to access the Twitter web API. 
 */
-export default async function handler(req, res) {
-  const twitter_api_endpoint = "https://api.twitter.com/2/tweets?";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const twitter_api_endpoint: string = "https://api.twitter.com/2/tweets?";
 
   if (req.method === "POST") {
     const { status_id } = req.body;
@@ -16,7 +19,7 @@ export default async function handler(req, res) {
       "user.fields": "profile_image_url,verified",
     };
 
-    let api_req_link =
+    let api_req_link: string =
       twitter_api_endpoint + new URLSearchParams(query_params).toString(); // Append status ID
 
     const api_request = {
@@ -26,8 +29,8 @@ export default async function handler(req, res) {
       },
     };
 
-    const api_response = await fetch(api_req_link, api_request);
-    const jsonData = await api_response.json();
+    const api_response: Response = await fetch(api_req_link, api_request);
+    const jsonData:string = await api_response.json();
 
     res.status(200).json(jsonData);
   }
