dce91e1c228adbbdf784d2366fb5c96f0b9e4129
Peter Georgas <peter_georgas@yahoo.com>
2021-12-26 10:46:14 -0500

    Updated CORS supported HTTP methods

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -3,7 +3,7 @@ import Cors from 'cors';
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ['GET', 'HEAD'],
+  methods: ['GET', 'POST'],
 })
 
 // Helper method to wait for a middleware to execute before continuing
