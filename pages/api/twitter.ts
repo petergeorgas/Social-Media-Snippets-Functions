6672088ea5442704e254992adbca9c059cecd035
Peter Georgas <peter_georgas@yahoo.com>
2022-04-16 23:28:02 -0400

    Began extracting profile pic image URL from API response.

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -1,24 +1,24 @@
-import type {VercelRequest, VercelResponse} from "@vercel/node"
-import Cors from 'cors';
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import Cors from "cors";
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ['GET', 'POST'],
-  origin: "*",
-})
+	methods: ["GET", "POST"],
+	origin: "*",
+});
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 function runMiddleware(req: VercelRequest, res: VercelResponse, fn: Function) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
 
-      return resolve(result)
-    })
-  })
+			return resolve(result);
+		});
+	});
 }
 
 /*
@@ -28,43 +28,49 @@ function runMiddleware(req: VercelRequest, res: VercelResponse, fn: Function) {
 */
 
 type QueryParams = {
-  ids: string;
-  expansions: string;
-  "tweet.fields": string;
-  "user.fields": string;
+	ids: string;
+	expansions: string;
+	"tweet.fields": string;
+	"user.fields": string;
 };
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await runMiddleware(req, res, cors)
-  
-  const twitter_api_endpoint: string = "https://api.twitter.com/2/tweets?";
-  console.log(`Request received! Method: ${req.method}`)
-  if (req.method === "POST") {
-    const {status_id}: {status_id: string} = req.body;
+	await runMiddleware(req, res, cors);
 
-    const query_params: QueryParams = {
-      "ids": status_id,
-      "expansions": "author_id",
-      "tweet.fields": "created_at,public_metrics,possibly_sensitive,in_reply_to_user_id,geo,entities",
-      "user.fields": "profile_image_url,verified",
-    };
+	const twitter_api_endpoint: string = "https://api.twitter.com/2/tweets?";
+	console.log(`Request received! Method: ${req.method}`);
+	if (req.method === "POST") {
+		const { status_id }: { status_id: string } = req.body;
 
-    let api_req_link: string =
-      twitter_api_endpoint + new URLSearchParams(query_params).toString(); // Append status ID
+		const query_params: QueryParams = {
+			ids: status_id,
+			expansions: "author_id",
+			"tweet.fields":
+				"created_at,public_metrics,possibly_sensitive,in_reply_to_user_id,geo,entities",
+			"user.fields": "profile_image_url,verified",
+		};
 
-    const twitter_api_request: RequestInit = {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.TWITTER_API_TOKEN}`,
-      },
-    };
+		let api_req_link: string =
+			twitter_api_endpoint + new URLSearchParams(query_params).toString(); // Append status ID
 
-    const twitter_api_response: Response = await fetch(api_req_link, twitter_api_request);
-    const jsonData: string = await twitter_api_response.json();
+		const twitter_api_request: RequestInit = {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${process.env.TWITTER_API_TOKEN}`,
+			},
+		};
 
+		const twitter_api_response: Response = await fetch(
+			api_req_link,
+			twitter_api_request
+		);
 
-    res.status(200).json(jsonData);
-  } else {
-    res.status(405).json({error: `${req.method} unsupported.`})
-  }
+		const api_resp = await twitter_api_response.json();
+		const pfp_link = api_resp["includes"]["users"][0]["profile_image_url"];
+
+		res.status(200).json(api_resp);
+		console.debug(`${pfp_link}`);
+	} else {
+		res.status(405).json({ error: `${req.method} unsupported.` });
+	}
 }
